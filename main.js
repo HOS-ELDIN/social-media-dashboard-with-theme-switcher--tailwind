d60bfcabfbea597567e553dedae2d7317f3619a0
HOSSAM-ELDIN <HOS-ELDIN@users.noreply.github.com>
2023-03-06 08:17:21 +0200

    a

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
-// let darkModeToggle = document.querySelector("label")
 let darkModeToggle = document.querySelector("#dark");
 let myPage = document.querySelector("html");
 darkModeToggle.onchange = () => {
-	console.log("checked");
 	myPage.classList.toggle("dark");
 };
